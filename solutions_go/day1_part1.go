451fc48ad892c87bc025d713668c6de7b15226f7
Bhav Khurana <bhav.khurana.cse21@itbhu.ac.in>
2023-12-01 11:15:24 +0530

    format code

--- solutions_go/day1_part1.go
+++ solutions_go/day1_part1.go
@@ -1,37 +1,38 @@
 package main
+
 import (
-  "fmt"
-  "unicode"
+	"fmt"
+	"unicode"
 )
 
 func main() {
-  
-  var ans int
-  
-  for true {
-    
-      var inp string
-      fmt.Scanln(&inp)
-      if inp == "" {
-          break
-      }
-      number := 0
-      for _,c := range inp {
-        if unicode.IsDigit(c) {
-          number += (int(c) - '0')*10
-          break
-        }
-      }
-      l := len(inp)
-      for i := l-1; i >= 0; i-- {
-        if unicode.IsDigit(rune(inp[i])) {
-          number += (int(inp[i]) - '0')
-          break
-        }
-      } 
-      
-      ans += number
-  }
-  
-  fmt.Println(ans)
+
+	var ans int
+
+	for true {
+
+		var inp string
+		fmt.Scanln(&inp)
+		if inp == "" {
+			break
+		}
+		number := 0
+		for _, c := range inp {
+			if unicode.IsDigit(c) {
+				number += (int(c) - '0') * 10
+				break
+			}
+		}
+		l := len(inp)
+		for i := l - 1; i >= 0; i-- {
+			if unicode.IsDigit(rune(inp[i])) {
+				number += (int(inp[i]) - '0')
+				break
+			}
+		}
+
+		ans += number
+	}
+
+	fmt.Println(ans)
 }
